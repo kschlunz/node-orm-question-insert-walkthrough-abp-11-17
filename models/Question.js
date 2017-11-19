50a798a416aeebc0e7a915b76f481b86357a901d
Kate Schlunz <schlunzk@gmail.com>
2017-11-19 16:49:03 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -25,7 +25,7 @@ class Question{
     return new Promise(function(resolve){
       db.run(sql, [self.content], function(err, result){
         self.id = this.lastID
-        resolve("Row inserted!")
+        resolve(self)
       })
 
     })
