0b36906489092f1d52e573e192a20af18facc899
Kate Schlunz <schlunzk@gmail.com>
2017-11-19 16:43:15 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -19,8 +19,13 @@ class Question{
   }
 
   insert(){
+    const sql = `INSERT INTO questions (content) VALUES (?)`
+
     return new Promise(function(resolve){
-      resolve("This DOes nothing!")
+      db.run(sql, [this.content], function(err, result){
+        resolve("Row inserted!")
+      })
+      
     })
   }
 
